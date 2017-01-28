84966f61f6dc5687ee9b093f4b22f395f015b193
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-27 21:57:57 -0600

    added register modal and basica layout styling

--- components/loginController.js
+++ components/loginController.js
@@ -9,10 +9,14 @@ console.log("login")
 
 
 $('#loginButton').click(() => {
-  $('.modal').modal('open')
+  $('#loginModal').modal('open')
 })
 
- $('.modal').modal({
+$('#registerButton').click(() => {
+  $('#registerModal').modal('open')
+})
+
+ $('#loginModal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .3, // Opacity of modal background
       inDuration: 700, // Transition in duration
@@ -23,7 +27,22 @@ $('#loginButton').click(() => {
 
         console.log(modal, trigger);
       },
-      complete: function() { alert('Closed'); } // Callback for Modal close
+      complete: function() { console.log('Closed'); } // Callback for Modal close
+    }
+  );
+
+ $('#registerModal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .3, // Opacity of modal background
+      inDuration: 700, // Transition in duration
+      outDuration: 700, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '20%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+
+        console.log(modal, trigger);
+      },
+      complete: function() { console.log('Closed'); } // Callback for Modal close
     }
   );
 
