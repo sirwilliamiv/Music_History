3f7f835e8b30bdc7fafbb947bb39356828680191
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-27 21:18:20 -0600

    added collapsable menu for playlists

--- User/userController.js
+++ User/userController.js
@@ -1,4 +1,10 @@
 app.controller('userController', function($scope, $location){
+
 console.log("user")
 
+  $('.collapsible').collapsible({
+    accordion: false, // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    onOpen: function(el) { console.log('Open'); }, // Callback for Collapsible open
+    onClose: function(el) { console.log('Closed'); } // Callback for Collapsible close
+  });
 })
